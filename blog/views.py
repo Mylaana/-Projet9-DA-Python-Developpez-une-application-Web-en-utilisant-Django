8fe51a13bde8b1e722b16d62f0017ade745da05d
Mylaana <alexandre.thore34@gmail.com>
2023-08-07 15:38:04 +0200

    update global css et construction page flux

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def flux_page(request):
-    return render(request, "blog/flux.html")
+    ticket_list = [
+        {"id": 1, "is_answer": True, "is_answered": False, "is_open_to_comments": False, "title": "titre 1", "author": "auteur 1", "comment": "commentaire 1", "rating": "4/5", "image_link": "", "post_date": "07/08/2023", "original_ticket": {"id": 3, "is_answer": False, "is_answered": True, "is_oppenned": False, "title": "titre 3", "author": "auteur 3", "comment": "commentaire 3", "original_ticket": ""}},
+        {"id": 2, "is_answer": False, "is_answered": False, "is_open_to_comments": True, "title": "titre 2", "author": "auteur 2", "comment": "commentaire 2", "rating": "5/5", "image_link": "", "post_date": "06/08/2023", "original_ticket": ""},
+        {"id": 3, "is_answer": False, "is_answered": True, "is_open_to_comments": False, "title": "titre 3", "author": "auteur 3", "comment": "commentaire 3", "rating": "3/5", "image_link": "", "post_date": "05/08/2023", "original_ticket": ""},
+        {"id": 4, "is_answer": False, "is_answered": False, "is_open_to_comments": False, "title": "titre 4", "author": "auteur 4", "comment": "commentaire 4", "rating": "1/5", "image_link": "", "post_date": "04/08/2023", "original_ticket": ""},
+    ]
+    return render(request, "blog/flux.html", context={"ticket_list": ticket_list})
 
 
 def posts_page(request):
