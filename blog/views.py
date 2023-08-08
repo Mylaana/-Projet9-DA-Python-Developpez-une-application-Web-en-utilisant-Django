6fff8100b702b31fcfd50350313e5ed1af3a639d
Mylaana <alexandre.thore34@gmail.com>
2023-08-08 12:03:28 +0200

    cleaning

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,13 @@
+import copy
 from django.shortcuts import render
 from blog.models import UserFollows, Review, Ticket
-import copy
+
 
 def flux_page(request):
-    """ticket_list = [
-        {"is_answered": False, "is_open_to_review": False, "title": "titre 1", "user": "auteur 1", "description": "commentaire 1", "image_link": "", "time_created": "07/08/2023", "review": {"ticket": "", "is_answered": True, "headline": "r titre 3", "body": "r body 3", "user": "r user 3", "rating": "r 4/5", "time_created": "r 07/08/2023"}},
-        {"is_answered": False, "is_open_to_review": True, "title": "titre 2", "user": "auteur 2", "description": "commentaire 2", "image_link": "", "time_created": "06/08/2023", "original_ticket": None},
-        {"is_answered": True, "is_open_to_review": False, "title": "titre 3", "user": "auteur 3", "description": "commentaire 3", "image_link": "", "time_created": "05/08/2023", "original_ticket": None},
-        {"is_answered": False, "is_open_to_review": False, "title": "titre 4", "user": "auteur 4", "description": "commentaire 4", "image_link": "", "time_created": "04/08/2023", "original_ticket": None},
-    ]"""
+    """
+    getting all tickets and their related reviews formatted as such : 
+    [{ticket: {ticket.values,..},review: {review.values,..}}]
+    """
     tickets_with_reviews = []
     for ticket in Ticket.objects.all():
         reviews_data = []
