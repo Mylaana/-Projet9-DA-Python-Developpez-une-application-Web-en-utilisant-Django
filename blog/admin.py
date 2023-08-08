5d751c912443012196387079b6421621203d8ebd
Mylaana <alexandre.thore34@gmail.com>
2023-08-08 11:33:35 +0200

    commit before merge

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from blog.models import Ticket, Review, UserFollows
 
-# Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "user", "image", "time_created")
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("ticket", "rating", "user", "headline", "body", "time_created")
+
+
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ("user", "followed_user")
+
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollows, UserFollowsAdmin)
